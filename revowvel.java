d5bc3adac1e9430d2025210f27b13babd9170a3d
vangalkarthi <vangalkarthi@gmail.com>
2017-06-21 20:26:17 +0530

    Add files via upload

---
+++ revowvel.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class revowel
+{
+   public static void main(String args[])
+   {
+       String str,reverse="";
+       Scanner scan = new Scanner(System.in);
+       
+       System.out.print("Enter a String : ");
+       str = scan.nextLine();
+	
+	 int length = str.length();
+ 
+      for ( int i = length - 1 ; i >= 0 ; i-- )
+         reverse = reverse + str.charAt(i);
+ 
+       String str1 = reverse.replaceAll("[aeiouAEIOU]", "");
+       System.out.print(reverse);
+   }
+}
